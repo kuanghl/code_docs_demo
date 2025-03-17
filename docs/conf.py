4b5fff42260968351fbd0b0857359428b8ec0734
kuanghl <kuanghl1998@163.com>
2025-03-17 17:32:43 +0800

    fix(rtd): rtd generate docs in servers

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,34 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir, project_name):
+
+	with open('Doxyfile.in', 'r') as file :
+		filedata = file.read()
+  
+	filedata = filedata.replace('@DOXYGEN_PROJECT_NAME@', project_name)
+	filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+	filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+ 
+	with open('Doxyfile', 'w') as file:
+		file.write(filedata)
+  
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+if read_the_docs_build:
+	input_dir = '../include'
+	output_dir = 'build'
+	project_name = 'code_docs_demo'
+	configureDoxyfile(input_dir, output_dir, project_name)
+	subprocess.call('doxygen', shell=True)
+	breathe_projects['code_docs_demo'] = output_dir + '/xml'
+
+# -- Project information for CMake -----------------------------------------------------
+ 
 project = 'code_docs_demo'
 copyright = '2023, Thilina Ratnayaka'
 author = 'Thilina Ratnayaka'
